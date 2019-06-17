d71e2b0420d119dd5ba68cb79c737c392397ad66
Julio Eduardo Pastore Baires <julio.pastore.jp@gmail.com>
2019-06-16 19:19:55 -0600

    Mapa de prueba

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,5 +8,28 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+
+  map: Map;
+
+  ionViewDidEnter() { this.leafletMap(); }
+
+  leafletMap() {
+    // In setView add latLng and zoom
+    this.map = new Map('mapId').setView([28.644800, 77.216721], 10);
+    tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'edupala.com © ionic LeafLet',
+    }).addTo(this.map);
+
+
+    marker([28.6, 77]).addTo(this.map)
+      .bindPopup('Ionic 4 <br> Leaflet.')
+      .openPopup();
+  }
+
+  /** Remove map when we have multiple map object */
+  ionViewWillLeave() {
+    this.map.remove();
+  }
 
 }
